be3c8dd89abb9201ec12e39880274ff94a9d0a32
Max <maxr1876@yahoo.com>
2016-10-10 15:15:10 -0600

    added attributes and constructor to property class

--- A4/src/a4/monopoly/Property.java
+++ A4/src/a4/monopoly/Property.java
@@ -1,5 +1,22 @@
 package a4.monopoly;
 
 public class Property {
-
+	private double listedPrice;
+	private String name;
+	private Player owner;
+	private double mortgageValue;
+	private int hotelsOwned;
+	private int housesOwned;
+	private boolean availToBuy;
+	
+	
+	public Property(double listedPrice, String name, double mortgageValue){
+		this.listedPrice = listedPrice;
+		this.name = name;
+		this.mortgageValue = mortgageValue;
+		this.owner = null;
+		this.hotelsOwned = 0;
+		this.housesOwned = 0;
+		this.availToBuy = true;
+	}
 }
