fb4f65638633824399ccaa535c868245fb714daf
Max <maxr1876@yahoo.com>
2016-10-10 15:50:47 -0600

    fix

--- A4/src/a4/monopoly/Board.java
+++ A4/src/a4/monopoly/Board.java
@@ -6,11 +6,5 @@ public class Board {
 	private Property locations[] = new Property [40];
 	private Random random;
 	
-	
-	public int [] rollDice(){
-		int die1 = random.nextInt(5)+1;
-		int die2 = random.nextInt(5)+1;
-		int [] roll = {die1, die2};
-		return roll;
-	}
+
 }
